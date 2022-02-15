777dd298e3783a7c5acb52c12a8396089888f3fa
GaoHang <catblack2021@163.com>
2022-02-15 08:10:10 +0800

    implementMove commit

--- dataType.js
+++ dataType.js
@@ -8,4 +8,12 @@ function createThing(){
     console.log("hello gitee");
     return ;
 }
+function implmentMove(){
+    let xLocation = document.scrollingElement;
+    for(let i=0;i<10;i++)
+        xLocation + 6;
+    if(xLocation>10)
+        let yLocation = 50,zLocation = 60;
+    console.log(xLocation);
+}
 createThing();
